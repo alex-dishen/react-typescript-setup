c1068bb51ff02f7f4d209ea3388410a932e407c3
Alex <didyshen.oleksandr@gmail.com>
2023-01-04 20:30:17 +0200

    Add typescript types for

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
 import App from './App';
+import GlobalStyle from './styles/GlobalStyle';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <React.StrictMode>
+    <GlobalStyle />
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
 );
