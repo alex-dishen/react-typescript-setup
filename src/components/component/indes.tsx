98334798deec6ad07bad885f6e285c671a5147db
Oleksandr <didyshen.oleksandr@gmail.com>
2023-05-30 16:03:13 +0300

    Add cli for components

--- src/components/component/indes.tsx
+++
@@ -1,6 +0,0 @@
-import { ExampleProps } from 'components/component/types';
-import { StyledExample } from 'components/component/styles';
-
-const Example = () => {};
-
-export default Example;
--- src/hooks/useExample/index.ts
+++
@@ -1,4 +0,0 @@
-import { useState, useEffect } from 'react';
-import { useExampleTypes } from 'hooks/useExample/types';
-
-export const useExample = () => {};
